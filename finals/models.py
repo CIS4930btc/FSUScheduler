9ca2d20a353b8fa1a84834d88d5e0509bf0bb657
bethany sanders <bethany.sanders33@gmail.com>
2017-07-27 09:35:20 -0400

    added days of week dropdown and time validation

--- finals/models.py
+++ finals/models.py
@@ -5,12 +5,17 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 
+DAY_CHOICES = (
+    ("MWF", "Monday, Wednesday, Friday"),
+    ("TR", "Tuesday, Thursday"),
+)
+
 # Create your models here.
 class Final(models.Model):
     user_name = models.ForeignKey(settings.AUTH_USER_MODEL)
     class_name = models.CharField(max_length=100)
-    class_day = models.CharField(max_length=100)
-    class_time = models.CharField(max_length=100)
+    class_day = models.CharField(max_length=100, choices = DAY_CHOICES, default = "MWF")
+    class_time = models.TimeField()
 
     def __str__(self):
         return self.class_name
